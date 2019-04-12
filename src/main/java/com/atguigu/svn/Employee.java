130ae38d555b024de113c9f0bcbaeb5882bcd04d
harry <harry@qq.com>
2019-04-12 18:19:57 +0800

    新建的工程

---
+++ src/main/java/com/atguigu/svn/Employee.java
@@ -0,0 +1,11 @@
+package com.atguigu.svn;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+	}
+
+}
